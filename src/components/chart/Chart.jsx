02092aaaf3e0e30fcc632f691740277b4afb594a
Kelvin <kel7.dev@gmail.com>
2023-08-10 06:49:32 -0400

    adding files

---
+++ src/components/chart/Chart.jsx
@@ -0,0 +1,28 @@
+import ChartBar from "./ChartBar";
+
+import "./Chart.css";
+
+function Chart(props) {
+  const dataPointValues = props.dataPoints.map((dataPoint) => {
+    return dataPoint.value;
+  });
+
+  const totalMaximum = Math.max(...dataPointValues);
+
+  return (
+    <div className="chart">
+      {props.dataPoints.map((dataPoint) => {
+        return (
+          <ChartBar
+            key={dataPoint.label}
+            value={dataPoint.value}
+            maxValue={totalMaximum}
+            label={dataPoint.label}
+          />
+        );
+      })}
+    </div>
+  );
+}
+
+export default Chart;
